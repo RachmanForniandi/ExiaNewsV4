f1c519d742be31658040333ad3f003f49c6e9ce7
RachmanForniandi <rachmanforniandi@gmail.com>
2022-07-26 01:10:20 +0700

    set menu & navigation for fragments done.

--- app/src/main/java/rachman/forniandi/exianewsv4/HomeActivity.kt
+++ app/src/main/java/rachman/forniandi/exianewsv4/HomeActivity.kt
@@ -2,6 +2,11 @@ package rachman.forniandi.exianewsv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import rachman.forniandi.exianewsv4.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
@@ -12,6 +17,15 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_home)
         setContentView(binding.root)
+        val navController = findNavController(R.id.nav_host_fragment)
+        /*val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_bookmark
+            )
+        )
+        setupActionBarWithNavController(navController,appBarConfiguration)*/
+        binding.navView.setupWithNavController(navController)
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING)
 
     }
 }
